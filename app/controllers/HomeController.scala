162e33daa9ed4bddefb2b4612775646864f611cf
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-11-10 11:07:57 +0100

    Refactoring landing page.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -17,7 +17,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Welcome to FarmDiagnostics."))
+    Ok(views.html.index("Welcome to Farm Diagnostics."))
   }
 
 }
