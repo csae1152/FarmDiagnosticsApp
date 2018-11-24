aebec3e8cf3b56d936cc276232248b27731cd27c
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-11-24 10:24:02 +0100

    Adds new title.

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -1,5 +1,11 @@
 package controllers
 
-class DashboardController {
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
 
+@Singleton
+class DashboardController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def dashboard = Action {
+    Ok(views.html.dashboard());
+  }
 }
